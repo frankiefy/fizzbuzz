34b34e7efa6601c76ec5e98b8e8a8a02175b71d3
frankie <franklin.shaw@gmail.com>
2016-12-14 16:26:25 +0000

    added new tests(5,15) and fizzbuzz is functional

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 ==0
+    return 'fizzbuzz'
+  elsif number % 5 == 0
+    return 'buzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  end
+end
